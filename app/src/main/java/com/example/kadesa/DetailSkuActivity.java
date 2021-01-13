2679bd3438bf5fc3e48655ae50b0aba2dbf75b24
Andhika Effendy <andhikaeffendy14@gmail.com>
2021-01-13 13:30:26 +0700

    detail sku update

--- app/src/main/java/com/example/kadesa/DetailSkuActivity.java
+++ app/src/main/java/com/example/kadesa/DetailSkuActivity.java
@@ -10,5 +10,9 @@ public class DetailSkuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_sku);
+
+        getSupportActionBar().setTitle("Detail Permohonan SKU");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
